db4666ef5754c65d5c8b771b8cc175a8ecd43814
mostafa21v <mo9085477@gmail.com>
2024-12-15 06:55:09 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,7 @@
+function togglemenu() {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+}
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
